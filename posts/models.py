707cd616e8418aa107c6170846b2ad3de3fd4f03
roque_mc_harvey <msroque1@studet.hau.edu.ph>
2025-08-30 20:14:45 +0800

    1st

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import random
+from django.utils.text import slugify
+import os
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -17,9 +19,20 @@ class Post(models.Model):
     user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to=upload_image_path, blank=True, null=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.content[:50]) or str(random.randint(1000, 9999))
+            slug = base
+            counter = 1
+            while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Post by {self.user.username} on {self.created_at}"
